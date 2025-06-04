d2710faef9984fdfaab032f5cc713d63f24c98c9
FIXED-TERM Kompella Ganesh Sai Akhil (SX/EAD3-MM) <gcp3kor@bosch.com>
2025-06-04 11:46:19 +0530

    Refactor existing nodes to use BaseNode abstraction

--- frontend/src/nodes/llmNode.js
+++ frontend/src/nodes/llmNode.js
@@ -1,34 +1,24 @@
 // llmNode.js
 
-import { Handle, Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const LLMNode = ({ id, data }) => {
-
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <Handle
-        type="target"
-        position={Position.Left}
-        id={`${id}-system`}
-        style={{top: `${100/3}%`}}
-      />
-      <Handle
-        type="target"
-        position={Position.Left}
-        id={`${id}-prompt`}
-        style={{top: `${200/3}%`}}
-      />
-      <div>
-        <span>LLM</span>
-      </div>
+    <BaseNode
+      id={id}
+      data={data}
+      title="LLM"
+      inputs={[
+        { id: "system", position: "33.3%" },
+        { id: "prompt", position: "66.6%" }
+      ]}
+      outputs={[
+        { id: "response", position: "50%" }
+      ]}
+    >
       <div>
         <span>This is a LLM.</span>
       </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-response`}
-      />
-    </div>
+    </BaseNode>
   );
 }
